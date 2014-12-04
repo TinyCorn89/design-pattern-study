37c63575bed398f8aa42ea34069dbf08da23cb9b
yamazaki <yamazaki.shinya@fujixerox.co.jp>
2014-12-04 17:55:42 +0900

    Revert "update 07_Builder"
    This reverts commit 03bcb6692abec142c68a0453517e14e391fe85d9.

--- 04_FactoryMethod/sample/idcard/IDCard.java
+++ 04_FactoryMethod/sample/idcard/IDCard.java
@@ -1,7 +1,7 @@
 package idcard;
 import framework.*;
 
-public class IDCard extend Product {
+public class IDCard extends Product {
 	private String owner;
 	IDCard(String owner) {
 		System.out.println(owner + "のカードを作ります。");
