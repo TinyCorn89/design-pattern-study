1636030165fae9871727d0855f4e80ca6ef1f71e
t.yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2014-12-17 16:26:16 +0900

    finish to create "code sample". later, change chobitto.

--- 15_Facade/Main2.java
+++ 15_Facade/Main2.java
@@ -2,6 +2,6 @@ import pagemaker.PageMaker;
 
 public class Main2{
 	public static void main (String[] args){
-		pageMakert.makeLinkPage("linkPage.html");
+		PageMaker.makeLinkPage("linkPage.html");
 	}
 }
